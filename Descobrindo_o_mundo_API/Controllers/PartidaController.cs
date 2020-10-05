7544ae84baa63f78710b573ae1ea85c8e8d161a2
lucasmoraessilva <lucasmoraessilva01@gmail.com>
2020-10-05 12:50:54 -0300

    Tratamento de exceções feito na cotroller de partidas, no método de listar partidas

---
+++ Descobrindo_o_mundo_API/Controllers/PartidaController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Descobrindo_o_mundo_API;
+using Descobrindo_o_mundo_API.Models;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Descobrindo_o_mundo_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PartidaController : ControllerBase
+    {
+        //api/[controller]/{nickname}
+        [HttpGet("{nickname}")]
+        public ActionResult<List<Partida>> Lista(string nickname)
+        {
+            try
+            {
+                descobrindo_mundoContext _db = new descobrindo_mundoContext();
+                var listaTblPartida = _db.TblPartida.Where(x => x.IdPacientePartidaNavigation.DscNicknamePaciente == nickname).ToList();
+                List<Partida> listaPartida = new List<Partida>();
+                foreach (var tblPartida in listaTblPartida)
+                {
+                    Partida partida = new Partida(tblPartida.IdJogoPartida, tblPartida.IdPacientePartida, tblPartida.IdPalavraPartida, tblPartida.DtPartida.ToString(), tblPartida.DuracaoPartida.ToString(), tblPartida.StatusPartida, (int)tblPartida.QtdErrosPartida, (int)tblPartida.QtdAcertosPartida);
+                    listaPartida.Add(partida);
+                }
+                return Ok(listaPartida);
+            }
+            catch (Exception)
+            {
+
+                return StatusCode(
+                        500,
+                        new ErrorResponse("Não foi possível responder a requisição.")
+                    );
+            }
+        }
+
+        //api/[controller]
+        [HttpPost]
+        public ActionResult Cadastrar([FromBody] Partida partida)
+        {
+            partida.Cadastrar(partida);
+            return Ok();
+        }
+    }
+}
---
+++ Descobrindo_o_mundo_API/Models/Partida.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Razor.Language.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Descobrindo_o_mundo_API.Models
+{
+    public class Partida
+    {
+        #region Propriedades
+        private int _id;
+        private int _idJogo;
+        private int _idPaciente;
+        private int _idPalavra;
+        private DateTime _data;
+        private DateTime _duracao;
+        private string _status;
+        private int _qtdErros;
+        private int _qtdAcertos;
+        #endregion
+
+        #region Properties
+        public int IdPartida { get => _id; set => _id = value; }
+        public int IdJogo { get => _idJogo; set => _idJogo = value; }
+        public int IdPaciente { get => _idPaciente; set => _idPaciente = value; }
+        public int IdPalavra { get => _idPalavra; set => _idPalavra = value; }
+        public string Data { get => _data.ToString(); set => _data = DateTime.Parse(value); }
+        public string Duracao { get => _duracao.ToString(); set => _duracao = DateTime.Parse(value); }
+        public string Status { get => _status; set => _status = value; }
+        public int QtdErros { get => _qtdErros; set => _qtdErros = value; }
+        public int QtdAcertos { get => _qtdAcertos; set => _qtdAcertos = value; }
+        #endregion
+
+        #region Construtores
+        public Partida(int idJogo,int idPaciente,int idPalavra,string data,string duracao,string status,int qtdErros,int qtdAcertos)
+        {
+            this.IdJogo = idJogo;
+            this.IdPaciente = idPaciente;
+            this.IdPalavra = idPalavra;
+            this.Data = data;
+            this.Duracao = duracao;
+            this.Status = status;
+            this.QtdErros = qtdErros;
+            this.QtdAcertos = qtdAcertos;
+        }
+        #endregion
+
+        #region Métodos
+        public void Cadastrar(Partida partida)
+        {
+
+        }
+        #endregion
+    }
+}
